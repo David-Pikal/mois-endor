de4e054b7b00b5261e9c7ff308248dc3abb26868
David-Pikal <david.pikal0@gmail.com>
2022-04-10 16:17:38 +0200

    Discovery service added.

---
+++ discovery-service/src/test/java/cz/uhk/mos/endor/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.mos.endor.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
