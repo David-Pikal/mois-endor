62c159cef20db64ec887b24e88b60aacc93dd6bb
David-Pikal <david.pikal0@gmail.com>
2022-04-10 15:57:55 +0200

    Added Eureka client in .net microservice.

--- Backend/Endor.IncomeExpensesService/Program.cs
+++ Backend/Endor.IncomeExpensesService/Program.cs
@@ -1,11 +1,15 @@
 using Endor.IncomeExpensesService.Database;
 using Microsoft.EntityFrameworkCore;
+using Steeltoe.Discovery.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// discoverable by eureka server
+builder.Services.AddDiscoveryClient(builder.Configuration);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
