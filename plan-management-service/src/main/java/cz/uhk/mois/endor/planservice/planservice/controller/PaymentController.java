bb81c175de234007235007d178c89fc1c1e56b19
Ondra <ondrej.habr97@gmail.com>
2022-04-19 20:35:22 +0200

    finalize payment controller

--- plan-management-service/src/main/java/cz/uhk/mois/endor/planservice/planservice/controller/PaymentController.java
+++ plan-management-service/src/main/java/cz/uhk/mois/endor/planservice/planservice/controller/PaymentController.java
@@ -7,6 +7,7 @@ import cz.uhk.mois.endor.planservice.planservice.repository.ProjectRepository;
 import cz.uhk.mois.endor.planservice.planservice.util.Cycle;
 import cz.uhk.mois.endor.planservice.planservice.util.PaymentType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -47,8 +48,61 @@ public class PaymentController {
     }
 
     @GetMapping(path = "/all")
-    public Iterable<Payment> getAllPayments() {
-        return paymentRepository.findAll();
+    public Iterable<Payment> getAllUserPayments(@RequestParam Integer userID) {
+        return paymentRepository.findByUserID(userID);
+    }
+
+    @GetMapping(path = "/one")
+    public Optional<Payment> getOneUserPayment(@RequestParam Integer id) {
+        return paymentRepository.findById(id);
+    }
+
+    @PutMapping(path = "/update")
+    public String updatePayment(@RequestParam Integer id, @RequestParam String title, @RequestParam String value, @RequestParam String startDate, @RequestParam Integer userID,
+                                @RequestParam String endDate, @RequestParam String cycle, @RequestParam String paymentType, @RequestParam(required = false) Integer projectID) {
+
+        Date endD = null;
+        if (!endDate.equals("")) {
+            endD = Date.valueOf(endDate);
+        }
+
+        Project project = null;
+        if (projectID != null && projectID > 0) {
+            Optional<Project> optional = projectRepository.findById(projectID);
+            if (optional.isPresent()) {
+                project = optional.get();
+            }
+        }
+
+        Optional<Payment> optionalP = paymentRepository.findById(id);
+        if (optionalP.isEmpty()) {
+            return "id does not exist";
+        }
+
+        Payment payment = optionalP.get();
+
+        payment.setTitle(title);
+        payment.setValue(new BigDecimal(value));
+        payment.setStartDate(Date.valueOf(startDate));
+        payment.setEndDate(endD);
+        payment.setUserID(userID);
+        payment.setCycle(Cycle.valueOf(cycle.toUpperCase()));
+        payment.setPaymentType(PaymentType.valueOf(paymentType.toUpperCase()));
+        payment.setProject(project);
+
+        paymentRepository.save(payment);
+
+        return "Saved";
+    }
+
+    @DeleteMapping(path = "/delete")
+    public String deleteUserPayment(@RequestParam Integer id) {
+        try {
+            paymentRepository.deleteById(id);
+            return "deleted";
+        } catch (EmptyResultDataAccessException e) {
+            return "id does not exist";
+        }
     }
 
 }
--- plan-management-service/src/main/java/cz/uhk/mois/endor/planservice/planservice/repository/PaymentRepository.java
+++ plan-management-service/src/main/java/cz/uhk/mois/endor/planservice/planservice/repository/PaymentRepository.java
@@ -3,5 +3,8 @@ package cz.uhk.mois.endor.planservice.planservice.repository;
 import cz.uhk.mois.endor.planservice.planservice.model.Payment;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends CrudRepository<Payment, Integer> {
+    List<Payment> findByUserID(Integer userID);
 }
