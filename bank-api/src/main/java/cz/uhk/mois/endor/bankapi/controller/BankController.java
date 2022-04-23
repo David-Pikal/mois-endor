2cf2d67549e3b22e228aa6486c787927f30925e7
David-Pikal <david.pikal0@gmail.com>
2022-04-23 12:23:55 +0200

    Payment summary in controller.

--- bank-api/src/main/java/cz/uhk/mois/endor/bankapi/controller/BankController.java
+++ bank-api/src/main/java/cz/uhk/mois/endor/bankapi/controller/BankController.java
@@ -1,8 +1,8 @@
 package cz.uhk.mois.endor.bankapi.controller;
 
-import cz.uhk.mois.endor.bankapi.service.BankApiFeign;
 import cz.uhk.mois.endor.bankapi.model.payment.Payment;
 import cz.uhk.mois.endor.bankapi.model.transaction.Transaction;
+import cz.uhk.mois.endor.bankapi.service.BankApiFeign;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Slf4j
@@ -22,13 +25,55 @@ public class BankController {
 
     @GetMapping("/payment")
     public List<Payment> paymentList(@RequestParam String dateFrom, @RequestParam String dateTo, @RequestParam int accountId) {
+        // get all payments between given dates
+        var rawPayments = proxy.getPayments(dateFrom, dateTo, accountId);
+
         log.info(String.format("Get payments from %s, to %s.", dateFrom, dateTo));
+
+        var sum = new BigDecimal(0);
+
+        for (Payment payment : rawPayments) {
+            // 2012-04-23T18:25:43.511Z
+            // parse Strings - need only date part
+            var firstPayment = LocalDate.parse(payment.recuringPayment().firstPayment().substring(0,10));
+            var lastPayment = LocalDate.parse(payment.recuringPayment().lastPayment().substring(0,10));
+
+            long numberOfPayments = 0;
+            switch (payment.recuringPayment().interval()) {
+                // enhanced switch - do not need break statement
+                case WEEK -> {
+                    numberOfPayments = ChronoUnit.WEEKS.between(firstPayment, lastPayment);
+                    log.info(String.format("Number of weeks between %s and %s is: %s.", firstPayment, lastPayment, numberOfPayments));
+                }
+                case MONTH -> {
+                    numberOfPayments = ChronoUnit.MONTHS.between(firstPayment, lastPayment);
+                    log.info(String.format("Number of months between %s and %s is: %s.", firstPayment, lastPayment, numberOfPayments));
+                }
+                case YEAR -> {
+                    numberOfPayments = ChronoUnit.YEARS.between(firstPayment, lastPayment);
+                    log.info(String.format("Number of years between %s and %s is: %s.", firstPayment, lastPayment, numberOfPayments));
+                }
+                default -> log.error("Payment do not have Interval!");
+            }
+            sum = sum.add(new BigDecimal(payment.value().amount() * (numberOfPayments + 1)));
+            log.info(String.format("Big decimal: %s", sum.toEngineeringString()));
+        }
+
+        // TODO - return value
         return proxy.getPayments(dateFrom, dateTo, accountId);
     }
 
     @GetMapping("/transaction")
     public List<Transaction> transactionList(@RequestParam String dateFrom, @RequestParam String dateTo, @RequestParam int accountId) {
         log.info(String.format("Get transactions from %s, to %s.", dateFrom, dateTo));
+        var sum = new BigDecimal(0);
+        var rawTransactions = proxy.getTransactions(dateFrom, dateTo, accountId);
+
+        // TODO sum transactions
+        for (Transaction transaction : rawTransactions) {
+            log.info(transaction.toString());
+        }
+
         return proxy.getTransactions(dateFrom, dateTo, accountId);
     }
 
--- bank-api/src/main/java/cz/uhk/mois/endor/bankapi/model/payment/RecuringPayment.java
+++ bank-api/src/main/java/cz/uhk/mois/endor/bankapi/model/payment/RecuringPayment.java
@@ -3,5 +3,5 @@ package cz.uhk.mois.endor.bankapi.model.payment;
 public record RecuringPayment(
     String firstPayment,
     String lastPayment,
-    String interval
+    Interval interval
 ) {}
